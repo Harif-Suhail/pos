83a783421f24367085306658bb92c88d2b42fdbb
harif suhail <harifsuhailp@gmail.com>
2025-11-17 17:49:43 +0530

    feat: wrap Toast component in a div for improved layout control

--- components/common/Toast.tsx
+++ components/common/Toast.tsx
@@ -51,7 +51,9 @@ const ToastContainer: React.FC<ToastContainerProps> = ({ toasts }) => {
         <div aria-live="assertive" className="fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start z-[100]">
             <div className="w-full flex flex-col items-center space-y-4 sm:items-end">
                 {toasts.map(toast => (
-                    <Toast key={toast.id} toast={toast} />
+                    <div key={toast.id} className="max-w-sm w-full">
+                        <Toast toast={toast} />
+                    </div>
                 ))}
             </div>
         </div>
